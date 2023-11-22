112ce9f7ea841196f8e533db51dde748868b6635
Shadi-Almohtaseb <shadi876543211@gmail.com>
2023-11-22 20:27:04 +0200

    BACKEND_URL

--- app.ts
+++ app.ts
@@ -19,7 +19,7 @@ const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: process.env.BACKEND_URL,
   credentials: true
 }));
 
