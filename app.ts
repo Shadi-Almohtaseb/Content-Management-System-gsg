ce062d65645b42bd39e0664da3c4fc130fe21dea
Shadi-Almohtaseb <shadi876543211@gmail.com>
2023-11-22 20:30:33 +0200

    fix

--- app.ts
+++ app.ts
@@ -19,7 +19,7 @@ const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 app.use(cors({
-  origin: process.env.BACKEND_URL,
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
   credentials: true
 }));
 
